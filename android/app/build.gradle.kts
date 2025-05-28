494a2adc60f81fb1655c6f3023f73de2ee7d0840
Irfan Arfianto <82969405+irfanarfianto@users.noreply.github.com>
2025-05-28 08:55:29 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,10 +19,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.app_face_recognition"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,12 +27,13 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+        getByName("release") {
             signingConfig = signingConfigs.getByName("debug")
-            minifyEnabled true
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
@@ -44,8 +41,8 @@ android {
 flutter {
     source = "../.."
 }
-dependencies {
-    // ... dependencies lain ...
 
-    implementation 'org.tensorflow:tensorflow-lite-gpu:2.12.0' // contoh versi, sesuaikan versi yang kamu pakai
+dependencies {
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.12.0")
+    // Tambahkan dependency lain di sini sesuai kebutuhan
 }
